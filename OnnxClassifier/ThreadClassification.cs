9c82c724a83b426ffd25ecfb25c5e87de99748f4
alexandraroots <korneevasandra@gmail.com>
2020-09-30 21:35:42 +0300

    Initial commit Task_1

---
+++ OnnxClassifier/ThreadClassification.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections.Concurrent;
+using System.Threading;
+
+
+namespace OnnxClassifier
+{
+    public class ThreadClassification
+    {
+        private AutoResetEvent wait = new AutoResetEvent(true);
+
+        private ConcurrentQueue<string> pathImages = new ConcurrentQueue<string>();
+
+        private CancellationTokenSource cancelThreads = new CancellationTokenSource();
+        private CancellationTokenSource cancelRun = new CancellationTokenSource();
+
+        private OnnxClassifier model;
+
+        public ThreadClassification(string currentDirectory, OnnxClassifier onnxModel)
+        {
+            model = onnxModel;
+
+            pathImages = new ConcurrentQueue<string>(Directory.GetFiles(currentDirectory, "*.JPEG"));
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Press Enter to stop");
+
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                var th = new Thread(Worker);
+                th.Name = $"Thread {i}";
+                th.Start();
+            }
+
+            while (!cancelRun.Token.IsCancellationRequested)
+            {
+                if (Console.KeyAvailable && Console.ReadKey().Key == ConsoleKey.Enter)
+                {
+                    cancelThreads.Cancel();
+                    break;
+                }
+
+            }
+
+        }
+
+
+
+        private void Worker()
+        {
+            while (!cancelThreads.Token.IsCancellationRequested && pathImages.TryDequeue(out string image))
+            {
+
+                string result = model.PredictModel(OnnxClassifier.PreprocImage(image));
+
+                wait.WaitOne();
+                Thread.Sleep(500);
+                if (!cancelThreads.Token.IsCancellationRequested)
+                    Console.WriteLine($"{Thread.CurrentThread.Name}: {result}");
+                wait.Set();
+
+
+            }
+            cancelRun.Cancel();
+
+            
+
+        }
+    
+    
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using OnnxClassifier;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath;
+            if (args.Length == 0)
+                directoryPath = "..//..//s02170130/ImageNetSample";
+            else
+                directoryPath = args[0];
+
+            OnnxClassifier.OnnxClassifier onnxModel = new OnnxClassifier.OnnxClassifier();
+
+            ThreadClassification task_1 = new ThreadClassification(directoryPath, onnxModel);
+
+            task_1.Run();
+
+        }
+    }
+}
