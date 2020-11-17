dbb9d4fe0aac742d66ada741d5eb8b0b726dccc0
alexandraroots <korneevasandra@gmail.com>
2020-11-17 19:25:09 +0300

    modified Task_3

--- Task_3/ClassificationVM.cs
+++ Task_3/ClassificationVM.cs
@@ -5,11 +5,17 @@ using System.Threading;
 using Avalonia.Threading;
 using System.Linq;
 using System.ComponentModel;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Drawing;
+using System.Collections.Generic;
+using Avalonia.Media.Imaging;
+using System.Numerics;
 
 
+//Task_3
 
-
-namespace Task_2
+namespace Task_3
 {
     public class ClassificationVM : INotifyPropertyChanged
     {
@@ -20,6 +26,12 @@ namespace Task_2
         public ObservableCollection<Tuple<string, int>> Classes {get; set;}
         public ObservableCollection<ResultClassification> Result { get; set; }
         public ObservableCollection<ResultClassification> SelectedResult { get; set; }
+
+        private ObservableCollection<Tuple<string, int>> stat { get; set; } 
+
+        private List<string> PathImages = new List<string>();
+        private ConcurrentQueue<string> NewPathImages = new ConcurrentQueue<string>();
+        private ApplicationContext db;
 
         private void OnPropertyChanged(string propertyName)
         {
@@ -46,16 +58,175 @@ namespace Task_2
                 
             }
         }
-
-        
+ 
 
         public ClassificationVM()
         {
             Classes = new ObservableCollection<Tuple<string, int>>();
             Result = new ObservableCollection<ResultClassification>();
             SelectedResult = new ObservableCollection<ResultClassification>();
+            stat = new ObservableCollection<Tuple<string, int>>();
+
+            db = new ApplicationContext();
 
             onnxModel = new OnnxClassifier.OnnxClassifier();
+
+        }
+
+        
+
+        public void RecognitionCompletedHandler(ResultClassification elem)
+        {
+            CreateResults(elem);
+            AddToDataBase(elem);
+        }
+
+        public void ExistsCompletedHandler(ResultClassification elem)
+        {
+            CreateResults(elem);
+            
+        }
+
+        public void CreateResults(ResultClassification elem)
+        {
+            Dispatcher.UIThread.InvokeAsync(() => {
+                Result.Add(elem);
+                int index = CheckUniqueClass(elem._ClassImage);
+                if (index == -1)
+                    Classes.Add(new Tuple<string, int>(elem._ClassImage, 1));
+                else
+                    Classes[index] = new Tuple<string, int>(Classes[index].Item1, Classes[index].Item2 + 1);
+            });
+        }
+
+
+        public void ClassificationImages(string currentDirectory)
+        {
+            Classes.Clear();
+
+            PathImages = new List<string>(Directory.GetFiles(currentDirectory, "*.jpg"));
+
+            Thread t = new Thread(() =>
+            {
+                foreach (string path in PathImages)
+                {
+                    ResultClassification imageDataBase = FindInDataBase(path);
+
+                    if (imageDataBase != null)
+                        ExistsCompletedHandler(imageDataBase);
+                    else
+                        NewPathImages.Enqueue(path);
+
+
+                }
+
+                task = new ThreadClassification(NewPathImages, onnxModel, RecognitionCompletedHandler);
+                task.Run();
+            });
+
+            t.Start();
+
+        }
+
+        public void StopClassificationImages()
+        {
+
+            task.Stopper();
+
+        }
+
+        ResultClassification FindInDataBase(string path)
+        {
+
+            byte[] array = ImageToByteArray(new Avalonia.Media.Imaging.Bitmap(path));
+
+
+            lock(db)
+            {
+                var images = db.Images.Where(p => p.Hash == GetHashFromBytes(array)).Select(p => p).ToList();
+                foreach (var img in images)
+                {
+                    db.Entry(img).Reference(p => p.ImageBytes).Load();
+                    if (array.SequenceEqual(img.ImageBytes.Bytes))
+                    {
+                        img.Call += 1;
+                        db.SaveChanges();
+                        return new ResultClassification(img.Path, img.Class, img.Prob);
+                    }
+                }
+            }
+            
+
+             return null; 
+        }
+
+
+        public void AddToDataBase(ResultClassification obj)
+        {
+            lock(db)
+            {
+                Avalonia.Media.Imaging.Bitmap BImage = new Avalonia.Media.Imaging.Bitmap(obj._PathImage);
+                Blob ImageBlob = new Blob { Bytes = ImageToByteArray(BImage) };
+
+                RecognitionImage elem = new RecognitionImage
+                {
+                    Path = obj._PathImage,
+                    Class = obj._ClassImage,
+                    Prob = obj.Probability,
+                    ImageBytes = ImageBlob,
+                    Call = 0,
+                    Hash = GetHashFromBytes(ImageToByteArray(BImage))
+                };
+                db.Images.Add(elem);
+                db.Blobs.Add(ImageBlob);
+
+                db.SaveChanges();
+
+            }
+        }
+
+        public void ClearDataBase()
+        {
+            lock (db)
+            {
+                try
+                {
+                    db.Database.EnsureDeleted();
+                    db.Database.EnsureCreated();
+                }
+                catch (Exception) { }
+            }
+
+        }
+
+        public byte[] ImageToByteArray(Avalonia.Media.Imaging.Bitmap image)
+        {
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                image.Save(stream);
+                return stream.ToArray();
+
+            }
+        }
+
+
+        public void Statistics()
+        {
+
+            stat.Clear();
+            lock(db)
+            {
+                var images = db.Images.Select(p => new
+                {
+                    Path = p.Path,
+                    Call = p.Call,
+                });
+
+                foreach (var elem in images)
+                    stat.Add(new Tuple<string, int>(elem.Path, elem.Call));
+            }
+
 
         }
 
@@ -64,7 +235,7 @@ namespace Task_2
             int index = -1;
             foreach (var item in Classes)
             {
-                
+
                 if (item.Item1.Equals(NewClass))
                 {
                     return Classes.IndexOf(item);
@@ -73,35 +244,9 @@ namespace Task_2
             return index;
         }
 
-        public void RecognitionCompletedHandler(ResultClassification elem)
+        public static int GetHashFromBytes(byte[] bytes)
         {
-            Dispatcher.UIThread.InvokeAsync(() => {
-                Result.Add(elem);
-                int index = CheckUniqueClass(elem._ClassImage);
-                    if (index == -1)
-                        Classes.Add(new Tuple<string, int>(elem._ClassImage, 1));
-                    else
-                        Classes[index] = new Tuple<string, int>(Classes[index].Item1, Classes[index].Item2 + 1);
-                });
-      
-
-        }
-
-        public void ClassificationImages(string path)
-        {
-            Classes.Clear();
-            task = new ThreadClassification(path, onnxModel, RecognitionCompletedHandler);
-            Thread t = new Thread(() => task.Run());
-            t.Start();
-
-
-        }
-
-        public void StopClassificationImages()
-        {
-            
-            task.Stopper();
-            
+            return new BigInteger(bytes).GetHashCode();
         }
     }
 }
